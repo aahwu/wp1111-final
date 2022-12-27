f0776777ae1f719cb997264fef90bbbc17dab80c
aahwu <shane880927@gmail.com>
2022-12-27 20:48:06 +0800

    [Upload] upload first version with graphQL

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -22,15 +22,93 @@ let books = [
   // ... Write a few more ...
 ]
 
-// const typeDefs = gql`
-//   type Book {
-//     id: ID!
-//     name: String
-//   }
-//   type Query {
-//     getBooks: [Book]
-//   }
-// `;
+const typeDefs = gql`
+  type Query {
+    kanbans(query: String): [Kanban!]
+    lists(query: String!): [List!]
+    # cards(query: String!): Kanban!
+  }
+  
+  type Mutation {
+    createKanban(data: CreateKanbanInput!): Kanban!
+    deleteKanban(id: String!): Kanban!
+    updateKanban(id: String!, data: UpdateKanbanInput): Kanban!
+    createList(data: CreateListInput!): List!
+    deleteList(id: ID!): List!
+    updateList(id: ID!, data: UpdateListInput): List!
+    createCard(data: CreateCardInput!): Card!
+    deleteCard(id: ID!): Card!
+    updateCard(id: ID!, data: UpdateCardInput): Card!
+  }
+  
+  # type Subscription {
+  #   card(listId: ID!): CardSubscriptionPayload!
+  #   list(kanbanId: ID!): ListSubscriptionPayload!
+  # }
+  
+  type Kanban {
+    _id: ID!
+    name: String!
+    lists: [List!]
+  }
+  
+  type List {
+    _id: ID!
+    parentId: String!
+    name: String!
+    cards: [Card!]
+  }
+  
+  type Card {
+    _id: ID!
+    parentId: String!
+    body: String!
+  }
+  
+  input CreateKanbanInput {
+    name: String!
+  }
+  
+  input UpdateKanbanInput {
+    name: String!
+  }
+  
+  input CreateListInput {
+    name: String!
+    kanbanId: String!
+  }
+  
+  input UpdateListInput {
+    parentId: String
+    name: String
+  }
+  
+  input CreateCardInput {
+    body: String!
+    listId: String!
+  }
+  
+  input UpdateCardInput {
+    parentId: String
+    body: String
+  }
+  
+  enum MutationType {
+    CREATED
+    UPDATED
+    DELETED
+  }
+  
+  type CardSubscriptionPayload {
+    mutation: MutationType!
+    data: Card!
+  }
+  
+  type ListSubscriptionPayload {
+    mutation: MutationType!
+    data: List!
+  }
+`;
 
 // const resolvers = {
 //   Query: {
@@ -42,7 +120,7 @@ let books = [
 const { serverRuntimeConfig } = getConfig();
 console.log(serverRuntimeConfig)
 const schemaPath = resolve(serverRuntimeConfig.PROJECT_ROOT, 'out/public/schema.graphql');
-const typeDefs = readFileSync(schemaPath, 'utf8');
+// const typeDefs = readFileSync(schemaPath, 'utf8');
 dbConnect();
 const cors = Cors();
 const apolloServer = new ApolloServer({
