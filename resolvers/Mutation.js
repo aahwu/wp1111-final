3e6225553ac462061afe613827f19c0486758530
aahwu <shane880927@gmail.com>
2023-01-03 01:05:44 +0800

    [Update] update user model and authentication

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -37,7 +37,7 @@ const Mutation = {
       return { payload: "FAIL", errorMsg: "Wrong password."}
     }
     console.log("success")
-    const newToken = await createToken({ id: userExist._id, name: userExist.name });
+    const newToken = jwt.sign({ id: userExist._id, name: userExist.name }, SECRET, {expiresIn: '1d'});
     console.log(newToken)
     return { 
       user: userExist, 
